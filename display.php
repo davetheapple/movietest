3c40dc86a863d819c2690ef625928a48a9807012
David <David@MooMoo.local>
2017-10-12 16:57:17 -0600

    display data

--- display.php
+++ display.php
@@ -37,16 +37,21 @@
         		<table datatable="" dt-options="ctrl.dtOptions" dt-columns="ctrl.dtColumns" class="row-border hover" ></table>
         		<!-- Modal -->
 				<div id="detail" class="modal fade" role="dialog" >
-					<div class="modal-dialog" style="background: url({{selected.backdrop_path}}) no-repeat; background-size: cover;">
+					<div class="modal-dialog">
 						<!-- Modal content-->
-						<div class="modal-content">
+						<div class="modal-content" style="background: url({{selected.backdrop_path}}) no-repeat; background-size: cover;">
 							<div class="modal-header">
 								<button type="button" class="close" data-dismiss="modal">&times;</button>
 								<h4 class="modal-title">Movie Details</h4>
 							</div>
 							<div class="modal-body">
-								<h3>{{selected.title}}</h3>
+								<h3>{{selected.title_reg}}</h3>
+								<div class="row">
+									<div class="col-md-4">
 								<img ng-src="{{selected.poster_path}}">
+							</div>
+							<div class="col-md-8"><p>{{selected.overview}}</p></div>
+							</div>
 								
 							</div>
 							<div class="modal-footer">
