f223ac5846d88d7c5232d806a369994e8235df55
David <David@MooMoo.local>
2017-10-12 17:55:49 -0600

    display data

--- display.php
+++ display.php
@@ -62,5 +62,8 @@
 				</div>
         	</div>
         </div>
+        <footer>
+        	<img src="https://www.themoviedb.org/assets/static_cache/9b3f9c24d9fd5f297ae433eb33d93514/images/v4/logos/408x161-powered-by-rectangle-green.png" width="300">
+        </footer>
     </body>
 </html>
